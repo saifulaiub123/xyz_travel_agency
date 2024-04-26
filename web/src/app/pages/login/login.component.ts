800c6e92c6299d570fbd8d3d576d173d99cafb4e
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-04-26 06:54:01 +0600

    modify

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../shared/header/header.component";
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [CommonModule, HeaderComponent, FooterComponent]
 })
 export class LoginComponent {
 
